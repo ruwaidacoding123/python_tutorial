827b8c4257aa5ec9f8c9afd5f9a9c9d74d1522b4
Ruweida <ruweidamuzamil075@gmail.com>
2024-08-03 21:00:53 +0300

    emoj conversion

---
+++ module4_Lists/emogeConvert.py
@@ -0,0 +1,14 @@
+message = input(">")
+words = message.split(' ')
+print(words)
+
+emojis = {
+    ":)": "😍",
+    ":)": "🕊️",
+    ":)": "❤️",
+    ":)": "😎"
+}
+output = ""
+for word in words:
+   output += emojis.get(word, word) + " "
+print(output)

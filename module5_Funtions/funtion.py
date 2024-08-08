d02549fd57a9a94002247c76435d780e479c2baa
Ruweida <ruweidamuzamil075@gmail.com>
2024-08-08 16:00:45 +0300

    function

--- module5_Funtions/funtion.py
+++ module5_Funtions/funtion.py
@@ -36,7 +36,7 @@ Arguments are the values passed inside the parenthesis of the function. A functi
 Example: Python Function with arguments
 In this example, we will create a simple function to check whether the number passed as an argument to the function is even or odd.
 '''
-# A simple Python function to check
+
 # whether x is even or odd
 def evenOdd(x):
 	if (x % 2 == 0):

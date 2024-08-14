9de49f96c5a04eaf0035ec0925dccef86c95e6e5
Ruweida <ruweidamuzamil075@gmail.com>
2024-08-14 17:57:01 +0300

    inheritence

---
+++ module5_Funtions/inheritence.py
@@ -0,0 +1,19 @@
+#inheritance is mechanism for using code and once again
+#is not limitede again
+
+class Mammal():
+    def walk(self):
+        print("walking")
+
+class Dogs():
+    pass
+
+class Cats():
+    def be_annoying(self):
+        print("annoying")
+
+cat1 = Cats()
+cat1.be_annoying
+
+
+        
